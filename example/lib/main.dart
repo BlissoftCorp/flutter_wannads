a3dcdb18195d67abcef524f5f97cc20622610ec6
Andres Gutierrez <andresgutice@hotmail.com>
2022-07-30 01:39:39 -0500

    Modificaciones

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   @override
   void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,17 +26,31 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Column(children: [ ElevatedButton(
-          onPressed: (){FlutterWannads.instance.init(
-            apiKey: "62e1a9405eaad229634630", 
-            apiSecret: "a4a55e0ab9", 
-            user: WanUser(id: "test"),
-            );},
-          child: const Text('Init Wannads'),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                FlutterWannads.instance.init(
+                  apiKey: "[ApiKey]",
+                  apiSecret: "[ApiSecret]",
+                  user: WanUser(id: "[Unique_User_Id]"),
+                );
+              },
+              child: const Text('Init Wannads'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  FlutterWannads.instance.showOfferwall();
+                },
+                child: const Text("Show Offerwall")),
+            ElevatedButton(
+              onPressed: () {
+                FlutterWannads.instance.showSurveysOfferwall();
+              },
+              child: const Text("Show SurveysOfferwall"),
+            )
+          ],
         ),
-        ElevatedButton(onPressed: (){FlutterWannads.instance.showOfferwall();}, child: const Text("Mostrar Offerwall")),
-                ElevatedButton(onPressed: (){FlutterWannads.instance.showSurveysOfferwall();}, child: const Text("Mostrar SurveysOfferwall"))
-        ],)
       ),
     );
   }
